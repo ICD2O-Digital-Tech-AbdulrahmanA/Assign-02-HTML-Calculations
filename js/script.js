9b3de8a06314c967e872ce8bd7d4feead729ee73
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-04-07 13:54:01 +0000

    calculations complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Abdul
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+
+// Function to calculate and display surface area and volume
+function calculateCylinder() {
+  // get radius and height values from user input 
+  let radius = parseFloat(document.getElementById("radius").value);
+  let height = parseFloat(document.getElementById("height").value);
+
+  // constants
+  const PI = Math.PI;
+
+  // surface area = 2πr² + 2πrh
+  const surfaceArea = 2 * PI * radius * radius + 2 * PI * radius * height;
+
+  // volume = πr²h
+  const volume = PI * radius * radius * height;
+
+  // round to 2 decimal place for display
+  const roundedSurfaceArea = surfaceArea.toFixed(2);
+  const roundedVolume = volume.toFixed(2);
+
+  // display results
+  document.getElementById("result").innerText = "Surface Area: " + roundedSurfaceArea + " cm² Volume: " + roundedVolume + " cm³";
+
+}
+
